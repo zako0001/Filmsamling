69ae01c12f0a09e476b460192f58d9f8c2893470
zako0001 <zako0001@stud.kea.dk>
2024-04-03 18:58:47 +0200

    refactored færdigt: menuer splittet op engelsk rettet til dansk

--- src/ui/UserInterface.java
+++ src/ui/UserInterface.java
@@ -2,6 +2,7 @@ package ui;
 
 import domain_model.*;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
@@ -11,14 +12,17 @@ public class UserInterface {
     // Attributes
     private final Controller controller;
     private final Scanner scanner;
-    private boolean running;
+    private final String[] movieAttributes;
+    private String searchWord;
+    private List<Movie> searchResults;
+    private List<Movie> deletedMovies;
 
     // Constructor
     public UserInterface() {
         controller = new Controller();
         scanner = new Scanner(System.in);
-        scanner.useDelimiter("\n"); // Forhindrer bøvl med mellemrum.
-        running = false;
+        scanner.useDelimiter("\n");
+        movieAttributes = new String[]{"Filmtitel", "Filminstruktør", "Årstal", "Farvefilm", "Længde i minutter", "Genre"};
 
         // Hardcodede film til hurtig brugertest af søgefunktion
         controller.addMovie("Avatar", "James Cameron", 2009, true, 162, "Action");
@@ -27,139 +31,183 @@ public class UserInterface {
         controller.addMovie("Harry Potter and the Prisoner of Azkaban", "Alfonso Cuarón", 2004, true, 142, "Adventure");
     }
 
-    // Main menu method
+    // Method
     public void startProgram() {
-        running = true;
         System.out.println("\nVelkommen til min filmsamling!");
-        while (running) {
-            System.out.println("\n---Hovedmenu---");
-            System.out.println("1. Opret en film");
-            System.out.println("2. Søg efter film");
-            System.out.println("3. Udskriv filmsamling");
-            System.out.println("4. Afslut");
-            System.out.print("\nIndtast menuvalg: ");
-            switch (scanIntSafely()) { // Menuvalg indtastes her.
-                case 1 -> addMovie();
-                case 2 -> searchMovie(); // Her kan fundne film ændres.
-                case 3 -> showMovieCollection();
-                case 4 -> stopProgram();
-                default -> System.out.println("Menuvalg skal være '1', '2', '3' eller '4'."); // Hvis der tastes ugyldigt tal.
-            }
+        mainMenu();
+    }
+
+    // Auxiliary methods
+    private void mainMenu() {
+        System.out.println("\n---Hovedmenu---");
+        System.out.println("1. Opret en film");
+        System.out.println("2. Søg efter film");
+        System.out.println("3. Udskriv filmsamling");
+        System.out.println("0. Afslut");
+        int option = scanInt("Indtast menuvalg", 0, 3);
+        switch (option) {
+            case 1 -> addMovie();
+            case 2 -> searchMovie();
+            case 3 -> showMovieCollection();
+            case 0 -> System.out.println("\nAfslutter...");
         }
     }
 
-    // Main menu option methods (private da de bare er hjælpemetoder)
     private void addMovie() {
-        System.out.println("\nIndtast venligst informationer om filmen.");
-        System.out.println("Title:");
-        String title = scanner.next();
-        System.out.println("Director:");
-        String director = scanner.next();
-        System.out.println("Year created:");
-        int yearCreated = scanIntSafely();
-        System.out.println("Is in color (yes/no):");
-        boolean isInColor = scanner.next().equalsIgnoreCase("yes");
-        System.out.println("Length in minutes:");
-        int lengthInMinutes = scanIntSafely();
-        System.out.println("Genre:");
-        String genre = scanner.next();
+        System.out.println("\nIndtast informationer om filmen.");
+        String title = scanString(movieAttributes[0]);
+        String director = scanString(movieAttributes[1]);
+        int yearCreated = scanInt(movieAttributes[2], 1800, 2100);
+        boolean isInColor = scanBoolean(movieAttributes[3]);
+        int lengthInMinutes = scanInt(movieAttributes[4], 0, Integer.MAX_VALUE);
+        String genre = scanString(movieAttributes[5]);
         controller.addMovie(title, director, yearCreated, isInColor, lengthInMinutes, genre);
-        System.out.println("\nFilm tilføjet.");
+        System.out.println("Film tilføjet.");
+        mainMenu();
     }
 
     private void searchMovie() {
-        System.out.println("\nIndtast søgeord til filmtitlerne:");
-        String searchTitle = scanner.next(); // Her er søgeordet.
-        List<Movie> movies = controller.searchMovie(searchTitle); // Her er listen over de film, der har søgeordet i titlen.
-        if (movies.isEmpty()) { // Tjekker om listen er tom, for så var der ingen matches på søgeordet.
-            System.out.println("No match for '" + searchTitle + "'.");
-        } else { // Hvis der er matches, skal de vises i en resultatmenu, hvor man kan vælge mellem dem.
-            while (true) { // While gør at vi kan komme tilbage til denne resultatmenu.
-                System.out.println("\n---Movies containing '" + searchTitle + "' in title---"); // Resultatmenuens overskrift.
-                int option = 0;
-                for (Movie movie : movies) { // Vi skal bygge resultatmenuen ud fra, hvor mange matches der er, så her går vi gennem dem.
-                    System.out.println(++option + ". " + movie.getTitle()); // Her bliver udskrevet et tal (valgmulighed) og en filmtitel.
-                    // ++ før variabelnavnet betyder, at variablen bliver 1 højere inden den læses.
-                }
-                System.out.println("0. Tilbage til hovedmenu");
-                System.out.println("\nIndtast valg af film:");
-                int chosenOption = scanIntSafely();
-                if (chosenOption > 0 && chosenOption <= option) { // Hvis en af filmene blev valgt (vi kan komme tilbage til resultatmenuen).
-                    Movie chosenFilm = movies.get(chosenOption - 1); // -1 skyldes at valgmulighederne starter fra 1 og lister starter fra 0, så filmene vil være 1 lavere i listen.
-                    // Der skal nu laves en filmændringsmenu ud fra den valgte film (se nederst i domain_model.Movie-klassen).
-                    System.out.println("\n---Valgte film---");
-                    System.out.println("1. Title: " + chosenFilm.getTitle());
-                    System.out.println("2. Director: " + chosenFilm.getDirector());
-                    System.out.println("3. Year created: " + chosenFilm.getYearCreated());
-                    System.out.println("4. Is in color: " + (chosenFilm.getIsInColor() ? "yes" : "no"));
-                    System.out.println("5. Length in minutes: " + chosenFilm.getLengthInMinutes());
-                    System.out.println("6. Genre: " + chosenFilm.getGenre());
-                    System.out.println("7. Slet filmen");
-                    System.out.println("0. Tilbage til søgeresultater");
-                    System.out.println("\nVil du ændre filmen?");
-                    System.out.print("Indtast valgmulighed: ");
-                    int editOption = scanIntSafely();
-                    if (editOption > 0 && editOption < 7) {
-                        System.out.print("Indtast ny information: ");
-                        switch (editOption) {
-                            case 1 -> chosenFilm.setTitle(scanner.next());
-                            case 2 -> chosenFilm.setDirector(scanner.next());
-                            case 3 -> chosenFilm.setYearCreated(scanIntSafely());
-                            case 4 -> chosenFilm.setIsInColor(scanner.next().equals("yes"));
-                            case 5 -> chosenFilm.setLengthInMinutes(scanIntSafely());
-                            case 6 -> chosenFilm.setGenre(scanner.next());
-                        }
-                        System.out.println("\n---Film ændret til---");
-                        showMovie(chosenFilm);
-                    } else if (editOption == 7) {
-                        System.out.println("Er du sikker på, at du vil slette '" + chosenFilm.getTitle() + "' permanent (ja/nej)?");
-                        if (scanner.next().equalsIgnoreCase("ja")) {
-                            System.out.println("'" + chosenFilm.getTitle() + "' er slettet fra filmsamlingen.");
-                            controller.deleteMovie(chosenFilm);
-                            break; // Bryder ud, fordi resultatmenuen indeholder en nu slettet film og fordi indekser kan være flyttet.
-                        } else {
-                            System.out.println("'" + chosenFilm.getTitle() + "' blev ikke slettet fra filmsamlingen.");
-                        }
-                    }
-                }
-                else { // Hvis ingen af filmene blev valgt
-                    break; // Bryder ud af while-loopet og dermed ud af resultatmenuen.
-                }
+        deletedMovies = new ArrayList<>();
+        System.out.print("Indtast søgeord til filmtitlerne: ");
+        searchWord = scanner.next();
+        searchResults = controller.searchMovie(searchWord);
+        if (searchResults.isEmpty()) {
+            System.out.println("Ingen resultater med søgeordet '" + searchWord + "' i titlen.");
+            mainMenu();
+        } else {
+            searchMenu();
+        }
+    }
+
+    private void searchMenu() {
+        System.out.println("\n---Resultater med søgeordet '" + searchWord + "' i titlen---");
+        int option = 0;
+        List<Integer> invalidOptions = new ArrayList<>();
+        for (Movie movie : searchResults) {
+            if (deletedMovies.contains(movie)) {
+                System.out.println("\u001B[9m" + ++option + ". " + movie.getTitle() + "\u001B[0m");
+                invalidOptions.add(option);
+            } else {
+                System.out.println(++option + ". " + movie.getTitle());
             }
+        }
+        System.out.println("0. Tilbage til hovedmenu");
+        int chosenOption;
+        while (true) {
+            chosenOption = scanInt("Indtast menuvalg", 0, option);
+            if (invalidOptions.contains(chosenOption)) {
+                System.out.println("Den film er blevet slettet.");
+            } else {
+                break;
+            }
+        }
+        if (chosenOption == 0) {
+            mainMenu();
+        } else {
+            Movie chosenMovie = searchResults.get(chosenOption - 1);
+            System.out.println("\n---Valgt film---");
+            editMenu(chosenMovie);
+        }
+    }
+
+    private void editMenu(Movie movie) {
+        showMovie(movie);
+        System.out.println("\n---Redigér eller slet '" + movie.getTitle() + "'---");
+        for (int i = 1; i < 7; i++) {
+            System.out.println(i + ". Redigér " + movieAttributes[i - 1].toLowerCase());
+        }
+        System.out.println("7. Slet filmen");
+        System.out.println("0. Tilbage til søgeresultater");
+        int choice = scanInt("Indtast menuvalg", 0, 7);
+        if (choice == 0) {
+            searchMenu();
+        } else if (choice == 7) {
+            deleteMovie(movie);
+        } else {
+            editMovie(choice - 1, movie);
+        }
+    }
+
+    private void editMovie(int attributeIndex, Movie movie) {
+        String message = "Indtast ny " + movieAttributes[attributeIndex].toLowerCase();
+        switch (attributeIndex) {
+            case 0 -> movie.setTitle(scanString(message));
+            case 1 -> movie.setDirector(scanString(message));
+            case 2 -> movie.setYearCreated(scanInt(message, 1800, 2100));
+            case 3 -> movie.setIsInColor(scanBoolean(message));
+            case 4 -> movie.setLengthInMinutes(scanInt(message, 0, Integer.MAX_VALUE));
+            case 5 -> movie.setGenre(scanString(message));
+        }
+        System.out.println("\n---Film ændret til---");
+        editMenu(movie);
+    }
+
+    private void deleteMovie(Movie movie) {
+        boolean delete = scanBoolean("Er du sikker på, du vil slette '" + movie.getTitle() + "' permanent?");
+        if (delete) {
+            System.out.println("'" + movie.getTitle() + "' blev slettet fra filmsamlingen.");
+            controller.deleteMovie(movie);
+            deletedMovies.add(movie);
+            searchMenu();
+        } else {
+            System.out.println("'" + movie.getTitle() + "' blev ikke slettet fra filmsamlingen.");
+            System.out.println("\n---Valgt film---");
+            editMenu(movie);
         }
     }
 
     private void showMovie(Movie movie) {
-        System.out.println("Title: " + movie.getTitle());
-        System.out.println("Director: " + movie.getDirector());
-        System.out.println("Year created: " + movie.getYearCreated());
-        System.out.println("Is in color: " + (movie.getIsInColor() ? "yes" : "no"));
-        System.out.println("Length in minutes: " + movie.getLengthInMinutes());
-        System.out.println("Genre: " + movie.getGenre());
+        String[] specificMovieAttributes = {movie.getTitle(), movie.getDirector(), "" + movie.getYearCreated(), movie.getIsInColor() ? "Ja" : "Nej", "" + movie.getLengthInMinutes(), movie.getGenre()};
+        for (int i = 0; i < 6; i++) {
+            System.out.println(movieAttributes[i] + ": " + specificMovieAttributes[i]);
+        }
     }
 
     private void showMovieCollection() {
-        System.out.println("\n---domain_model.MovieCollection begins---");
+        System.out.print("\n------Liste af film------");
         for (Movie movie : controller.searchMovie("")) {
             System.out.println();
             showMovie(movie);
         }
-        System.out.println("\n---domain_model.MovieCollection ends---");
+        mainMenu();
     }
 
-    private void stopProgram() {
-        System.out.println("\nAfslutter...");
-        running = false; // Stopper while-loopet i startProgram-metoden og grunden til at running er attribute i stedet for lokal variabel i startProgram (pga. scope).
-    }
-
-    private int scanIntSafely() {
+    private int scanInt(String message, int lowerBound, int upperBound) {
+        System.out.print(message + ": ");
         try {
-            return scanner.nextInt();
-        }
-        catch (InputMismatchException ime) {
-            scanner.nextLine(); // Scanneren skal lige forstå, at den nu skal være klar til at læse på en ny linje
+            int number = scanner.nextInt();
+            if (number >= lowerBound && number <= upperBound) {
+                return number;
+            } else {
+                if (lowerBound == Integer.MIN_VALUE) {
+                    System.out.println("Indtastede var ikke under " + upperBound + ". Prøv igen.");
+                } else if (upperBound == Integer.MAX_VALUE) {
+                    System.out.println("Indtastede var ikke over " + lowerBound + ". Prøv igen.");
+                } else {
+                    System.out.println("Indtastede var ikke mellem " + lowerBound + " og " + upperBound + ". Prøv igen.");
+                }
+            }
+        } catch (InputMismatchException ime) {
+            scanner.nextLine();
             System.out.println("Indtastede var ikke heltal. Prøv igen.");
-            return scanIntSafely(); // Rekursion: Metoden kalder sig selv, og starter dermed forfra med et nyt try!
         }
+        return scanInt(message, lowerBound, upperBound);
+    }
+
+    private boolean scanBoolean(String message) {
+        System.out.print(message + " (ja/nej): ");
+        String answer = scanner.next();
+        if (answer.equalsIgnoreCase("ja")) {
+            return true;
+        } else if (answer.equalsIgnoreCase("nej")) {
+            return false;
+        }
+        System.out.println("Indtastede var ikke 'ja' eller 'nej'. Prøv igen.");
+        return scanBoolean(message);
+    }
+
+    private String scanString(String message) {
+        System.out.print(message + ": ");
+        return scanner.next();
     }
 }
